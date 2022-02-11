753ea1fe276adcb0507d94865573bba97b84d159
ontocord <ontocord@gmail.com>
2022-02-11 11:11:44 -0500

    Update process.py

--- process.py
+++ process.py
@@ -381,7 +381,7 @@ class TextAugment:
   cache_dir = None
 
   def __init__(self, device=None, single_process=1, available_gpu_model=None, labse=None, ontology_manager=None, translation_pipelines=None, ner_model_name2pipelines=None, en_spacy_nlp=None, faker_en_list=None, qg=None, kenlm_model=None, cache_dir="~/.cache"):
-    if TextAgument.cache_dir is None: TextAugment.cache_dir = cache_dir
+    if TextAugment.cache_dir is None: TextAugment.cache_dir = cache_dir
     if device is not None:
       TextAugment.device = device
       if device == "cpu": 
@@ -399,7 +399,7 @@ class TextAugment:
       self.initializer(available_gpu_model=available_gpu_model, device=TextAugment.device, labse=labse, ontology_manager=ontology_manager, translation_pipelines=translation_pipelines, ner_model_name2pipelines=ner_model_name2pipelines, en_spacy_nlp=en_spacy_nlp, faker_en_list=faker_en_list, qg=qg, kenlm_model=kenlm_model, cache_dir=cache_dir)
     
   def initializer(self, device_id_by_proess_id=True, all_available_gpu_model=None, available_gpu_model=None, device=None,  labse=None, ontology_manager=None, translation_pipelines=None, ner_model_name2pipelines=None, en_spacy_nlp=None, faker_en_list=None, qg=None, kenlm_model=None, cache_dir="~/.cache"):
-    if TextAgument.cache_dir is None: TextAugment.cache_dir = cache_dir
+    if TextAugment.cache_dir is None: TextAugment.cache_dir = cache_dir
     if all_available_gpu_model is not None:
       TextAugmentGPUModel.available_gpu_models  = all_available_gpu_model
     if device is not None:
@@ -488,7 +488,7 @@ class TextAugment:
 
   @staticmethod
   def load_kenlm_model(store_model=True):
-      if TextAgument.cache_dir == None:
+      if TextAugment.cache_dir == None:
         cache_dir = "~/.cache"
       else:
         cache_dir = TextAgument.cache_dir
@@ -2759,6 +2759,11 @@ if __name__ == "__main__":
     parser.add_argument('-infile', dest='infile', type=str, help='file to load', default=None)
     parser.add_argument('-outfile', dest='outfile', type=str, help='file to save', default=None)
     parser.add_argument('-num_workers', dest='num_workers', type=int, help='Num of Workers', default = 1)
+    parser.add_argument('-do_spacy_only', dest='do_spacy_only', type=int, help='Wether to only apply a spacy model', default = 1)
+    parser.add_argument('-do_hf_ner_only', dest='do_hf_ner_only', type=int, help='Wether to only apply a huggingface NER model', default = 1)
+    #parser.add_argument('-do_ontology_only', dest='do_ontology_only', type=int, help='Wether to only use an ontology', default = 1)
+    parser.add_argument('-do_regex_only', dest='do_regex_only', type=int, help='Wether to only  apply regex models', default = 1)
+    parser.add_argument('-do_qg_rel_only', dest='do_qg_rel_only', type=int, help='Wether to only infer a relationship between PII entities based an question generation (EXPERIMENTAL)', default = 1)
     parser.add_argument('-do_spacy', dest='do_spacy', type=int, help='Wether or not to apply a spacy model', default = 1)
     parser.add_argument('-do_skip_src_lang_processing', dest='do_skip_src_lang_processing', type=int, help='Wether or not to skip NER for src_lang (assumes NER is already perfored in the data provided)', default = 0)
     parser.add_argument('-do_hf_ner', dest='do_hf_ner', type=int, help='Wether or not to apply a huggingface NER model', default = 1)
@@ -2769,19 +2774,56 @@ if __name__ == "__main__":
     parser.add_argument('-do_regex', dest='do_regex', type=int, help='Wether or not to apply regex models', default = 1)
     parser.add_argument('-do_cleanup', dest='do_cleanup', type=int, help='Wether or not to cleanup NERs that are just stopwords or small number', default = 1)
     parser.add_argument('-do_marian_mt', dest='do_marian_mt', type=int, help='Wether or not to use marianMT for translation instead of M2M100', default = 0)
+    parser.add_argument('-do_docs_trim', dest='do_docs_trim', type=int, help='Wether or not to filter out documents with no mentions of persons, or high ratios of junk, or CSAM', default = 0)
+    parser.add_argument('-do_kenlm', dest='do_kenlm', type=int, help='Wether or not to apply a KenLM model to decide if a name is a common person name', default = 1)
+    parser.add_argument('-do_qg_rel', dest='do_qg_rel', type=int, help='Wether or not to infer a relationship between PII entities based an question generation (EXPERIMENTAL)', default = 1)
     parser.add_argument('-num_words_per_chunk', dest='num_words_per_chunk', type=int, help='number of words per chunk', default=70)
     #parser.add_argument('-ontology_weight', dest='ontology_weight', type=float, help='batch size', default=0.85)
     parser.add_argument('-spacy_weight', dest='spacy_weight', type=float, help='weight given to a spacy decision', default=1.00)
     parser.add_argument('-hf_ner_weight', dest='hf_ner_weight', type=float, help='weight given to a hf model decision', default=1.25)
     parser.add_argument('-regex_weight', dest='regex_weight', type=float, help='weight given to a regex decision', default=1.5)
     parser.add_argument('-backtrans_weight', dest='backtrans_weight', type=float, help='weight given to back tranlation decisions', default=0.9)
-    parser.add_argument('-do_docs_trim', dest='do_docs_trim', type=int, help='Wether or not to filter out documents with no mentions of persons, or high ratios of junk, or CSAM', default = 1)
-    parser.add_argument('-do_kenlm', dest='do_kenlm', type=int, help='Wether or not to apply a KenLM model to decide if a name is a common person name', default = 1)
-    parser.add_argument('-do_qg_rel', dest='do_qg_rel', type=int, help='Wether or not to infer a relationship between PII entities based an question generation', default = 1)
     parser.add_argument('-aug_scope', dest='aug_scope', type=str, help='tag types for augmentation', default="ADDRESS,ORG,PERSON,LOC,ID")
     parser.add_argument('-anon_scope', dest='anon_scope', type=str, help='tag types for anonymization', default='PERSON,ID')
     parser.add_argument('-preload_cache', dest='preload_cache', action='store_true', help='Preload the cache of models and data', default = 0)
     args = parser.parse_args()
+    if args.do_spacy_only:
+      args.do_spacy = True
+      args.do_hf_ner = False
+      args.do_regex = False
+      args.do_qg_rel = False
+      args.do_backtrans = False
+      args.target_lang = args.src_lang
+      args.do_anonymization = False
+      args.do_augmentation = False
+    elif args.do_regex_only:
+      args.do_spacy = False
+      args.do_hf_ner = False
+      args.do_regex = True
+      args.do_qg_rel = False
+      args.do_backtrans = False
+      args.target_lang = args.src_lang
+      args.do_anonymization = False
+      args.do_augmentation = False
+    elif args.do_hf_ner_only:
+      args.do_spacy = False
+      args.do_hf_ner = True
+      args.do_regex = False
+      args.do_qg_rel = False
+      args.do_backtrans = False
+      args.target_lang = args.src_lang
+      args.do_anonymization = False
+      args.do_augmentation = False
+    elif args.do_qg_rel_only:
+      args.do_spacy = False
+      args.do_hf_ner = False
+      args.do_regex = False
+      args.do_backtrans = False
+      args.do_qg_rel = True
+      args.target_lang = args.src_lang
+      args.do_anonymization = False
+      args.do_augmentation = False
+
     args.anon_scope = set(args.anon_scope.split(","))
     args.aug_scope = set(args.aug_scope.split(","))
     src_lang = args.src_lang
