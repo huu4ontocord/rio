e3b6497dc958d925dd2e4b39577b6f8495d25dde
ChienVM <chien_vm@detomo.co.jp>
2022-02-21 22:48:06 +0900

    Expand vietnamese firstname

--- fake_names.py
+++ fake_names.py
@@ -8,9 +8,14 @@ bantu_surnames = ["Dlamini", "Gumede", "Hadebe", "Ilunga", "Kamau", "Khoza", "Lu
                   "Ncube", "Ndagire", "Ndhlovu", "Ndikumana", "Ndiritu", "Ndlovu", "Ndzinisa", "Ngcobo", "Nkomo",
                   "Nkosi", "Nkurunziza", "Radebe", "Tshabalala", "Tshivhumbe", "Vila"]
 #male and female
-vietnamese_firstnames = ["Anh", "Dung", "Hanh", "Hoa", "Hong", "Khanh", "Lan", "Liem", "Nhung", "Duy", "Xuan"]
-vietnamese_surnames = ["Nguyễn","Trần","Lê","Phạm","Hoàng","Huỳnh","Phan","Vũ","Võ", "Đặng","Bùi", "Đỗ", "Hồ", "Ngô", "Dương", "Lý"]
-""
+# vietnamese_firstnames = ["Anh", "Dung", "Hanh", "Hoa", "Hong", "Khanh", "Lan", "Liem", "Nhung", "Duy", "Xuan"]
+vietnamese_firstnames = ["Trọng", "Hiền", "Bích", "Duy", "Yến", "Sơn", "Oanh", "Mẫn", "Đông", "Lam", "Tín", "Nhi", "Phúc", "Ngọc", "Dương", "Loan", "Luân", "Nhàn", "Hiếu", "Băng", "Phát", "Chi", "Thịnh", "Di", "Tuấn", "Vi", "Hoa", "Diệu", "Tú", "Uyên", "Huy", "Văn", "Kỳ", "Thy", "Như", "Hằng", "Thông", "Minh", "Xuân", "Diệp", "Thúy", "Trang", "Hòa", "Trinh", "An", "Khang", "Thành", "Thiên", "Khôi", "Trâm", "Khiêm",
+                         "Nam", "Tân", "Hân", "Hương", "Hồng", "Vỹ", "Phụng", "Sang", "Mạnh", "Ý", "Bách", "Long", "Kha", "Phi", "Châu", "Trúc", "Kiệt", "Nhựt", "Thái", "Trường", "Duyên", "Diễm", "Vy", "Quỳnh", "Linh", "Giang", "Huyền", "Khanh", "Quân", "Hoàng", "Tiến", "Dũng", "Cường", "Vinh", "Hiển", "Hạnh", "Tùng", "Đại", "Quốc", "Bình", "Tâm", "Tuyết", "Lợi", "Phương", "Mỹ", "Liên", "Đức", "Vương", "Thi", "Công",
+                         "Thanh", "Toàn", "Hải", "Triết", "Thiện", "Thùy", "Lâm", "Anh", "Tài", "Thư", "Đạt", "Tuyền", "Đăng", "Hưng", "Khải", "Hào", "Lan", "Trung", "Trí", "Ly", "Tường", "Nga", "Phước", "Hậu", "Tiên", "Nhật", "Ngân", "Khuê", "Thắng", "Ánh", "Thơ", "Phong", "Nhung", "Hiệp", "Thủy", "Doanh", "Phú", "Mai", "Nhã", "Hùng", "Việt", "Mi", "Kiên", "Kiều", "Thuận", "Nhiên", "Dung", "Trà", "Thắm", "Đan", "Thương",
+                         "Đào", "Nguyệt", "Trân", "Khánh", "Kim", "Tuệ", "Nguyên", "Vĩ", "My", "Thảo", "Nghi", "Vũ", "Khoa", "Ân", "Danh", "Hà", "Phượng", "Quý", "Quang", "Quyên", "Vân", "Lộc", "Nhân", "Tấn", "Nghĩa", "Bảo"]
+                         # source female: https://kiencang.net/100-ten-nu-gioi-pho-bien/, male: https://kiencang.net/100-ten-nam-pho-bien-vietnam/
+vietnamese_surnames = ["Nguyễn", "Trần", "Lê", "Phạm", "Hoàng", "Huỳnh", "Phan", "Vũ", "Võ", "Đặng", "Bùi", "Đỗ", "Hồ", "Ngô", "Dương", "Lý"]
+
 #use english firstnames for now
 bengali_surnames  = ["Banerjee", "Bagchi", "Bhaduri", "Bhattacharjee", "Chakraborty", "Chatterjee", "Ganguly", "Goswami", "Ghoshal", "Lahiri", "Maitra", "Mukherjee", "Sanyal", "Chakraborty", "Bhattacharya", "Baidya", "Sengupta", "Dasgupta", "Duttagupta", "Gupta", "Sen-Sharma", "Basu", "Bose", "Dutta", "Ghosh", "Choudhury", "Guha", "Gain", "Mitra", "Singh", "Sinha", "Sen", "Pal", "De", "Dey", "Deb", "Dev", "Jana", "Palit", "Chanda", "Chandra", "Das", "Dam", "Kar", "Nandi", "Sarkar", "Nag", "Som"]
 
