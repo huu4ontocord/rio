cd023074c5bd8734c85096f3771d75948b25e782
Pierre Colombo <PierreColombo@users.noreply.github.com>
2022-03-02 20:45:50 +0100

    Update text_augment.py

--- text_augment.py
+++ text_augment.py
@@ -142,11 +142,7 @@ class TextAugmentDeviceModel:
       self.translation_pipelines  = {}
       self.translation_pipelines["facebook/m2m100_418M"] =  M2M100ForConditionalGeneration.from_pretrained("facebook/m2m100_418M").eval().half().to(self.device)
     seen = {}
-    print(src_langs)
-    print(target_langs+aug_langs)
-    print(zip(src_langs, target_langs+aug_langs))
-    print([(src_lang, target_lang) in zip(src_langs, target_langs+aug_langs)])
-    pairs = list(set([(src_lang, target_lang) in zip(src_langs, target_langs+aug_langs)] + [(target_lang, src_lang) in zip(src_langs, target_langs+aug_langs)]))
+    pairs = list(set([(src_lang, target_lang) for src_lang, target_lang in zip(src_langs, target_langs+aug_langs)] + [(target_lang, src_lang) for src_lang,target_lang in zip(src_langs, target_langs+aug_langs)]))
     for pair in pairs:
       if pair not in seen:
         model_name = marian_mt.get(pair)
