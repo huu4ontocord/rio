bdb4d3f61a1f8eebb290967c5d7a2815ec207d19
ontocord <ontocord@gmail.com>
2022-03-03 02:31:33 -0500

    Update kenlm_model_extensions.py

--- kenlm_model_extensions.py
+++ kenlm_model_extensions.py
@@ -10,7 +10,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-# from https://huggingface.co/edugp/kenlm/blob/main/model.py which is under the Apache 2 License
+# from Eduardo Gonzalez Ponferrada/edugp's repo: https://huggingface.co/edugp/kenlm/blob/main/model.py which is under the Apache 2 License
 # thank you edugp!!
 import os
 import re
