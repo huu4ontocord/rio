2c619494b5b55e140d29908fae04044621f9c89c
ontocord <ontocord@gmail.com>
2022-03-03 05:36:36 -0500

    Create cjk.py

---
+++ cjk.py
@@ -0,0 +1,12 @@
+import re
+def cjk_detect(texts):
+    # korean
+    if re.search("[\uac00-\ud7a3]", texts):
+        return "ko"
+    # japanese
+    if re.search("[\u3040-\u30ff]", texts):
+        return "ja"
+    # chinese
+    if re.search("[\u4e00-\u9FFF]", texts):
+        return "zh"
+    return None
