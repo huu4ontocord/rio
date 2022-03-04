4f5bb9f11be3922b05b2b6147b0e3696df265358
ontocord <ontocord@gmail.com>
2022-03-04 08:31:38 -0500

    Update pii_regexes.py

--- pii_regexes.py
+++ pii_regexes.py
@@ -1,4 +1,7 @@
 import regex_manager
+import pii_regexes_rulebase
+
 #For backwards compatability. Everything moved to regex_manager.
+regex_rulebase = pii_regexes_rulebase.regex_rulebase
 detect_ner_with_regex_and_context = regex_manager.detect_ner_with_regex_and_context
 
