516c8169e937e7c625f597056266f3b0961f75a4
Pierre Colombo <PierreColombo@users.noreply.github.com>
2022-03-06 15:01:49 +0100

    Create test_all_languages.sh

---
+++ test_all_languages.sh
@@ -0,0 +1,19 @@
+
+export CUTOFF=150
+export GPU_NUMBERS=4
+export NODES=2
+for LANG in ar bn ca en es eu fr hi id ig ny pt sn st sw ur vi xh yo zh zu;do
+      EXP_NAME=${LANG}_${NODES}_${GPU_NUMBERS}_${CUTOFF}
+      sbatch --job-name=${EXP_NAME} \
+        --account=six@gpu \
+        --gres=gpu:1 \
+        --no-requeue \
+        --nodes=${nodes} \
+        --cpus-per-task=10 \
+        --hint=nomultithread \
+        --time=20:00:00 \
+        --qos=qos_gpu-t4 \
+        --output=jobinfo/${EXP_NAME}_%j.out \
+        --error=jobinfo/${EXP_NAME}_%j.err \
+        --wrap="module purge; module load pytorch-gpu/py3/1.7.0 ;export HF_DATASETS_OFFLINE=1; export TRANSFORMERS_OFFLINE=1 ;time python process.py -src_lang ${LANG} -do_trans 0 -cutoff ${CUTOFF} -do_hf_ner 1 -do_spacy 1 -do_regex 1 -do_kenlm 1 -do_anonymization 1"
+    done
