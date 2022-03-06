30bb1e2a110283bfc5e2fa48fdcb73114d0746bf
Pierre Colombo <PierreColombo@users.noreply.github.com>
2022-03-06 15:48:03 +0100

    Update test_all_languages.sh

--- test_all_languages.sh
+++ test_all_languages.sh
@@ -15,5 +15,5 @@ for LANG in ar bn ca en es eu fr hi id ig ny pt sn st sw ur vi xh yo zh zu;do
         --qos=qos_gpu-t4 \
         --output=jobinfo/${EXP_NAME}_%j.out \
         --error=jobinfo/${EXP_NAME}_%j.err \
-        --wrap="module purge; module load pytorch-gpu/py3/1.7.0 ;export HF_DATASETS_OFFLINE=1; export TRANSFORMERS_OFFLINE=1 ;time python process.py -src_lang ${LANG} -num_workers 2  -do_trans 0 -cutoff ${CUTOFF} -do_hf_ner 1 -do_spacy 1 -do_regex 1 -do_kenlm 1 -do_anonymization 1"
+        --wrap="module purge; module load pytorch-gpu/py3/1.7.0 ;export HF_DATASETS_OFFLINE=1; export TRANSFORMERS_OFFLINE=1 ;time python process.py -src_lang ${LANG} -outfile=${EXP_NAME}.json -num_workers 2  -do_trans 0 -cutoff ${CUTOFF} -do_hf_ner 1 -do_spacy 1 -do_regex 1 -do_kenlm 1 -do_anonymization 1"
     done
