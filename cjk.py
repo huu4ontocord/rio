6f50413083b2d30e7dc5d769b64683d032ab1a2a
ontocord <ontocord@gmail.com>
2022-03-06 01:09:56 -0500

    Update cjk.py

--- cjk.py
+++ cjk.py
@@ -1,12 +1,15 @@
 import re
 def cjk_detect(texts):
+    # chinese
+    if re.search("[\u4e00-\u9FFF]", texts):
+        return "zh"
     # korean
     if re.search("[\uac00-\ud7a3]", texts):
         return "ko"
     # japanese
     if re.search("[\u3040-\u30ff]", texts):
         return "ja"
-    # chinese
-    if re.search("[\u4e00-\u9FFF]", texts):
-        return "zh"
+    # thai
+    if re.search("[\u0E01-\u0E5B]", texts):
+        return "th"
     return None
