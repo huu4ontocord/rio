deb660586fa096d7f17df7f7f8e968a9705be71d
ontocord <ontocord@gmail.com>
2022-03-09 11:21:23 -0500

    Update banned_words.py

--- banned_words.py
+++ banned_words.py
@@ -1,4 +1,4 @@
-
+# note: this list is used in conjunction with the flagged words. So words of a sexual nature + words below MIGHT indicate child sexual abuse materials (CSAM).
 # see https://www.researchgate.net/publication/259179064_Comparing_Methods_for_Detecting_Child_Exploitation_Content_Online for common CSAM words
 # http://antipaedo.lip6.fr/T12/keywords_cnrs.pdf - for top 20 from table 7 and 8 of the paper, plus other similar words , ignoring stopwords like "tuesday"
 # WARNING: Translations are probably not accurate. TODO to fix.
