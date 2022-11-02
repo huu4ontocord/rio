67b1f6772c2f973f1acb063f0c781c57285a59a7
ontocord <ontocord@gmail.com>
2022-11-01 17:02:09 -0700

    Update kenlm_manager.py

--- kenlm_manager.py
+++ kenlm_manager.py
@@ -1,3 +1,4 @@
+#@title KenLM code
 """
 Copyright, 2021-2022 Ontocord, LLC, and other authors of Muliwai, All rights reserved.
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -10,7 +11,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-# from Eduardo Gonzalez Ponferrada/edugp's repo: https://huggingface.co/edugp/kenlm/blob/main/model.py which is under the Apache 2 License
+# from https://github.com/piisa/muliwai/blob/main/kenlm_manager.py
+# which is based Eduardo Gonzalez Ponferrada/edugp's repo: https://huggingface.co/edugp/kenlm/blob/main/model.py which is under the Apache 2 License
+# which is also based on https://github.com/facebookresearch/cc_net/ which is under the MIT License
 # thank you edugp!!
 import os
 import re
@@ -27,6 +30,21 @@ kenlm_models = {
     'oscar': {},
     'mc4': {},
 }
+
+#NOTE: If you want to use the default cc_net kenlm wikipedia models, you will need to download them.
+#see https://github.com/facebookresearch/cc_net/blob/main/Makefile. These are default models if there aren't any from edugp
+ccnet_langs="af,ar,az,be,bg,bn,ca,cs,da,de,el,en,es,et,fa,fi,fr,gu,he,hi,hr,hu,hy,id,is,it,ja,ka,kk,km,kn,ko,lt,lv,mk,ml,mn,mr,my,ne,nl,no,pl,pt,ro,ru,uk,zh".split(",")
+def download_ccnet_sp_kenlm_models(lang):
+  if not os.path.exists(f"/kenlm_ccnet_wikipedia_models/{lang}.arpa.bin"):
+    os.system(f"wget -c  -P ./kenlm_ccnet_wikipedia_models http://dl.fbaipublicfiles.com/cc_net/lm/{lang}.arpa.bin")
+  if not os.path.exists(f"/kenlm_ccnet_wikipedia_models/{lang}.sp.model"):
+    print (f"wget -c  -P ./kenlm_ccnet_wikipedia_models http://dl.fbaipublicfiles.com/cc_net/lm/{lang}.sp.model")
+    os.system(f"wget -c  -P ./kenlm_ccnet_wikipedia_models http://dl.fbaipublicfiles.com/cc_net/lm/{lang}.sp.model")
+
+def get_kenlm_models_from_savedir(save_dir="/content/drive/Shareddrives/LAION/kenlm_ccnet_wikipedia_models"):
+  os.system(f"cp -rf {save_dir} ./kenlm_ccnet_wikipedia_models")
+
+# WOULD be good if we can create models based on cc100.
 
 # TODO figure out actual numbers. Also, add languge specific kenlm models. Check if there are variations b/c of
 #  gender, so we would have at least two patterns.
@@ -85,12 +103,13 @@ public_figure_kenlm_cutoff_map = {
            }
 }
 
-
+# TODO: Instead of defaulting to the ccnet models, we will want to pick and choose from the ccnet/edugp wikipedia model
 def load_kenlm_model(
         src_lang: str = "en",
         pretrained_models: list = ['wikipedia'],
         store_model: bool = True,
-        cache_dir: str = None,
+        cache_dir: str = "./kenlm_edugp_models",
+        default_kenlm_wikipedia: str = "./kenlm_ccnet_wikipedia_models"
 ) -> dict:
     """
     Load all supported kenlm model for source language. Consider if we want to use an LRU.
@@ -100,37 +119,41 @@ def load_kenlm_model(
         kenlm_models), 'Total of number kenlm models loads larger than supported kenlm models'
     src_lang = src_lang if src_lang in public_figure_kenlm_cutoff_map else "en"
     all_models = {}
-    model_files = ["arpa.bin", "sp.model", "sp.vocab"]
+    model_files = ["arpa.bin", "sp.model", ] # "sp.vocab"
     # cache to dir
     if cache_dir is None:
         cache_dir = os.path.expanduser('~') + "/.cache"
-
+    
     # check if pretrain model exist
     for model_type in pretrained_models:
-        if model_type not in kenlm_models.keys():
+        if src_lang in kenlm_models[model_type]:
+            all_models[model_type] = kenlm_models[model_type][src_lang]
+        elif model_type == "wikipedia" and os.path.exists(f"{default_kenlm_wikipedia}/{src_lang}.arpa.bin"):
+            model = KenlmModel(default_kenlm_wikipedia, src_lang, do_normalize_spacing_for_tok=True)
+            all_models[model_type] = model
+            if store_model:
+              kenlm_models[model_type][src_lang] = model
+        elif model_type not in kenlm_models.keys():
             warnings.warn(f"{model_type} pretrained model is not supported!")
         else:
-            if src_lang in kenlm_models[model_type]:
-                all_models[model_type] = kenlm_models[model_type][src_lang]
-            else:
-                os.system(f"mkdir -p {cache_dir}/{model_type}")
-                found = True
-                for model_file in model_files:
-                    if not os.path.exists(f"{cache_dir}/{model_type}/{src_lang}.{model_file}"):
-                        try:
-                            file_url = hf_hub_url(repo_id="edugp/kenlm",
-                                                  filename=f"{model_type}/{src_lang}.{model_file}")
-                            file = cached_download(file_url)
-                            os.system(f"ln -s {file} {cache_dir}/{model_type}/{src_lang}.{model_file}")
-                        except:
-                            warnings.warn(f'could not find model {src_lang}.{model_file}. will stop searching...')
-                            found = False
-                            break
-                if found:
-                    model = KenlmModel(f"{cache_dir}/{model_type}", src_lang)
-                    all_models[model_type] = model
-                    if store_model:
-                        kenlm_models[model_type][src_lang] = model
+            os.system(f"mkdir -p {cache_dir}/{model_type}")
+            found = True
+            for model_file in model_files:
+                if not os.path.exists(f"{cache_dir}/{model_type}/{src_lang}.{model_file}"):
+                    try:
+                        file_url = hf_hub_url(repo_id="edugp/kenlm",
+                                              filename=f"{model_type}/{src_lang}.{model_file}")
+                        file = cached_download(file_url)
+                        os.system(f"ln -s {file} {cache_dir}/{model_type}/{src_lang}.{model_file}")
+                    except:
+                        warnings.warn(f'could not find model {src_lang}.{model_file}. will stop searching...')
+                        found = False
+                        break
+            if found:
+                model = KenlmModel(f"{cache_dir}/{model_type}", src_lang)
+                all_models[model_type] = model
+                if store_model:
+                    kenlm_models[model_type][src_lang] = model
     return all_models
 
 
@@ -180,7 +203,7 @@ class SentencePiece:
         tokenized = self.sp.encode_as_pieces(text)
         return " ".join(tokenized)
 
-
+#see https://github.com/facebookresearch/cc_net/blob/main/cc_net/text_normalizer.py
 class KenlmModel:
     digit_re: re.Pattern = re.compile(r"\d")
     unicode_punct: Dict[str, str] = {
@@ -226,6 +249,10 @@ class KenlmModel:
     kenlm_model_dir = None
     sentence_piece_model_dir = None
 
+    # TODO: we are not doing the sacremoses tokenizer to get put spaces between escaped chars 
+    # but consider whether we should do this for the ccnet models 
+    # https://github.com/facebookresearch/cc_net/blob/bda555bd1cf1ee2e0b925363e62a61cd46c8b60d/cc_net/tokenizer.py#L23
+    # does it make a difference?
     def __init__(
             self,
             model_dataset: str,
@@ -234,10 +261,12 @@ class KenlmModel:
             remove_accents: bool = False,
             normalize_numbers: bool = True,
             punctuation: int = 1,
+            do_normalize_spacing_for_tok: bool = False,
     ):
         self.model_dataset = model_dataset
         self.model = kenlm.Model(os.path.join(self.model_dataset, f"{language}.arpa.bin"))
         self.tokenizer = SentencePiece(os.path.join(self.model_dataset, f"{language}.sp.model"))
+        self.do_normalize_spacing_for_tok = do_normalize_spacing_for_tok
         self.accent = remove_accents
         self.case = lower_case
         self.numbers = normalize_numbers
@@ -254,7 +283,7 @@ class KenlmModel:
             model_dataset,
             language,
             False,
-            False,
+            language  in {"en", "my"},
             True,
             1,
         )
@@ -272,6 +301,8 @@ class KenlmModel:
                 punct=self.punct,
             )
         # Tokenize (after normalizing): See https://github.com/facebookresearch/cc_net/blob/bda555bd1cf1ee2e0b925363e62a61cd46c8b60d/cc_net/mine.py#L352 for full pipeline
+        if self.do_normalize_spacing_for_tok:
+          doc = self.normalize_spacing_for_tok(doc)
         doc = self.tokenizer.do(doc)
         doc_log_score, doc_length = 0, 0
         for line in doc.split("\n"):
@@ -280,7 +311,7 @@ class KenlmModel:
             doc_log_score += log_score
             doc_length += length
         return round(self.pp(doc_log_score, doc_length), 1)
-
+    
     def normalize(
             self,
             line: str,
@@ -304,6 +335,82 @@ class KenlmModel:
             line = self.remove_unicode_punct(line)
         line = self.remove_non_printing_char(line)
         return line
+
+    @staticmethod
+    def normalize_spacing_for_tok(text: str, language: str = "en") -> str:
+      res = (
+          text.replace("\r", "")
+          # remove extra spaces
+          .replace("(", " (")
+          .replace(")", ") ")
+          .replace(" +", " ")
+      )
+      res = re.sub(r"\) ([\.\!\:\?\;\,])", r"\)\1", res)
+      res = res.replace("( ", "(").replace(" )", ")")
+      res = re.sub(r"(\d) \%", r"\1\%", res)
+      res = res.replace(" :", ":").replace(" ;", ";")
+      res = res.replace("`", "'").replace("''", ' " ')
+
+      res = (
+          res.replace("„", '"')
+          .replace("“", '"')
+          .replace("”", '"')
+          .replace("–", "-")
+          .replace("—", " - ")
+          .replace(" +", " ")
+          .replace("´", "'")
+          .replace("([a-z])‘([a-z])", r"\1'\2/")
+          .replace("([a-z])’([a-z])", r"\1'\2/")
+          .replace("‘", '"')
+          .replace("‚", '"')
+          .replace("’", '"')
+          .replace("''", '"')
+          .replace("´´", '"')
+          .replace("…", "...")
+          # French quotes
+          .replace(" « ", ' "')
+          .replace("« ", '"')
+          .replace("«", '"')
+          .replace(" » ", '" ')
+          .replace(" »", '"')
+          .replace("»", '"')
+          # handle pseudo-spaces
+          .replace(" %", "%")
+          .replace("nº ", "nº ")
+          .replace(" :", ":")
+          .replace(" ºC", " ºC")
+          .replace(" cm", " cm")
+          .replace(" ?", "?")
+          .replace(" !", "!")
+          .replace(" ;", ";")
+          .replace(", ", ", ")
+          .replace(" +", " ")
+          .replace("．", ". ")
+      )
+      # English "quotation," followed by comma, style
+      if language == "en":
+          res = re.sub(r"\"([,\.]+)", r"\1\"", res)
+      # Czech is confused
+      elif language == "cs" or language == "cz":
+          pass
+      # German/Spanish/French "quotation", followed by comma, style
+      else:
+          res = res.replace(',"', '",')
+          res = re.sub(
+              r"(\.+)\"(\s*[^<])", r"\"\1\2", res
+          )  # don't fix period at end of sentence
+
+      if (
+          language == "de"
+          or language == "es"
+          or language == "cz"
+          or language == "cs"
+          or language == "fr"
+      ):
+          res = re.sub(r"(\d) (\d)", r"\1,\2", res)
+      else:
+          res = re.sub(r"(\d) (\d)", r"\1.\2", res)
+      return res
 
     @staticmethod
     def strip_accents(line: str) -> str:
